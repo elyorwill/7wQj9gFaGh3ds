790120272dba1138f7ec83f41f0981925c4c4d2d
Elyor M <Elyor M>
2016-12-09 11:01:04 +0800

    config checks if localhost or online

--- inc/configAll.php
+++ inc/configAll.php
@@ -10,22 +10,24 @@ if(session_id() == '') {
     session_start();
 }
 
-//online
-$db_username = 'xst60icf6z';
-$db_password = 'apl73fny8GsksK02';
-$db_name = 'rentmarket';
-$db_host = 'aac9ct2vh1ez7i.chzjx9qi5ks8.ap-southeast-1.rds.amazonaws.com:3306';
 
-//offline
-// $db_username = 'root';
-// $db_password = '';
-// $db_name = 'rentmarket';
-// $db_host = 'localhost';
+$currenturl = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+
+if (strpos($currenturl,'7wQj9gFaGh3ds') !== false) {
+  //offline
+  $db_username = 'root';
+  $db_password = '';
+  $db_name = 'rentmarket';
+  $db_host = 'localhost';
+  $connecDB = mysqli_connect($db_host, $db_username, $db_password,$db_name) or die('could not connect to database');
+} else {
+  //AWS online
+  $connecDB = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
+}
 
 
-// $connecDB = mysqli_connect($db_host, $db_username, $db_password,$db_name) or die('could not connect to database');
 
-$connecDB = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
+
 
 $createdatabasesql = "CREATE DATABASE IF NOT EXISTS rentmarket";
 mysqli_query($connecDB,$createdatabasesql);
