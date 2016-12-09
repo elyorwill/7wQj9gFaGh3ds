becfed68678b89c25f26390034c50a18af2de2eb
Elyor M <Elyor M>
2016-12-09 10:32:47 +0800

    added config file

---
+++ inc/configAll.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @author Elyor
+ * @copyright 3 / 8 / 2016
+ */
+date_default_timezone_set("Asia/Kuala_Lumpur");
+
+if(session_id() == '') {
+    session_start();
+}
+
+
+$db_username = 'root';
+$db_password = '';
+$db_name = 'rentmarket';
+
+
+$db_host = 'localhost';
+$connecDB = mysqli_connect($db_host, $db_username, $db_password,$db_name) or die('could not connect to database');
+
+
+$createdatabasesql = "CREATE DATABASE IF NOT EXISTS rentmarket";
+mysqli_query($connecDB,$createdatabasesql);
+
+$_SESSION['shomeurl'] = 'http://localhost/rentmarket/7wQj9gFaGh3ds';
+
+
+if(isset($_SESSION['rmnotfymsg'])){
+    $notficationmsg = $_SESSION['rmnotfymsg'];
+    unset($_SESSION['rmnotfymsg']);
+}
+else
+$notficationmsg = '';
+
+
+?>
