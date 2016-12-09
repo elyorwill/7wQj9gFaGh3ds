309da2de56d9b691bb371f7e651984667b1378ef
Elyor M <Elyor M>
2016-12-09 14:12:26 +0800

    added phpmyadmin

--- dbtest.php
+++ dbtest.php
@@ -12,27 +12,6 @@ $mysqtablescol=array();
 $mysqtablescol = mysqli_fetch_array($showtables);
 
 
-
-$sqlrunresult='';
-
-if(isset($_POST['runsql'])){
- $sqlinject = $_POST['sql'];
-
- $runmysql = mysqli_query($connecDB,$sqlinject);
- if($runmysql){
-   $sqlrunresult .='SUCCESS!';
- }else {
-   $sqlrunresult .= mysqli_error($connecDB);
- }
-}
-
-
-//db info
-$dbinformation='<br>DB info:';
-$dbinformation .= '<br>Host: '.$db_host;
-$dbinformation .= '<br>Username: '.$db_username;
-$dbinformation .= '<br>Password: '.$db_password;
-$dbinformation .= '<br>DB: '.$db_name;
  ?>
 
  <html>
@@ -41,18 +20,11 @@ $dbinformation .= '<br>DB: '.$db_name;
 <body>
   <?php
 
-echo $sqlrunresult.'<hr>';
-
 echo '<pre>';
 print_r($mysqtablescol);
 echo '</pre>';
 
-echo $dbinformation;
    ?>
-<form action="?runsql" method="post">
-<label>Query:</label><br>
-<textarea name="sql"></textarea><br>
-<input name="runsql" type="submit"/>
-</form>
+
 </body>
  </html>
